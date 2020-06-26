ef38e2393fd631f5cdecb5d54635bd4a83e60774
Sudharsan K <sudharsan_1985@live.in>
2020-06-26 20:19:51 +0800

    Sample commit from surface.

---
+++ src/webparts/photoSync/common/AccessAndListCheck.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const AccessAndListCheck: React.FunctionComponent<{}> = (props) => {
+    return(
+        <div></div>
+    );
+};
+
+export default AccessAndListCheck;
